d737773e3bf48ac260fd33ecc37b0750d3a1a637
Mir Arsh Zaffar <145954675+mir-arsh@users.noreply.github.com>
2025-07-08 20:41:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll("button");
+
+let string = "";
+let array = Array.from(buttons);
+array.forEach(button => {
+    button.addEventListener('click', (evt) => {
+        if (evt.target.innerHTML == "=") {
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if (evt.target.innerHTML == "AC") {
+            string = "";
+            input.value = string;
+        }
+
+        else if (evt.target.innerHTML == "DEL") {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+
+        else {
+            string += evt.target.innerHTML;
+            input.value = string;
+        }
+    });
+});
